6daccba9a2caa50cba06b216a46dad00f8dd27d8
Matteo Mendula <matteo.mendula@myndoor.it>
2025-04-24 18:24:40 +0200

    added lgvit early exit transformer

---
+++ lgvit/accelerator_lgvit.m
@@ -0,0 +1,5 @@
+num_pes: 256
+l1_size_cstr: 1600
+l2_size_cstr: 8000
+noc_bw_cstr: 1000
+offchip_bw_cstr: 160
---
+++ lgvit/lgvit_model.m
@@ -0,0 +1,468 @@
+Network DeiTHighway {
+    Layer Embed_Conv { // Patch Embeddings: Conv2d(3, 768, kernel_size=(16, 16), stride=(16, 16))
+        Type: CONV
+        Stride { X: 16, Y: 16 }
+        Dimensions { K: 768, C: 3, R: 16, S: 16, Y: 224, X: 224 } // Input Y, X
+
+    }
+
+// === Encoder Layers (0-11) ===
+// Input to Attention/FFN layers (as CONV): Y=197, X=1
+
+    // --- Layer 0 ---
+    Layer Enc0_Attn_Q { // Represents: Linear(768, 768) -> GEMM M=197, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    Layer Enc0_Attn_K { // Represents: Linear(768, 768) -> GEMM M=197, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    Layer Enc0_Attn_V { // Represents: Linear(768, 768) -> GEMM M=197, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    // Note: Attention score calculation (Q @ K.T) and value aggregation (Scores @ V) GEMMs omitted.
+
+    Layer Enc0_Attn_Output { // Represents: Linear(768, 768) -> GEMM M=197, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    Layer Enc0_FFN1 { // Represents: Linear(768, 3072) -> GEMM M=197, K=768, N=3072
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    Layer Enc0_FFN2 { // Represents: Linear(3072, 768) -> GEMM M=197, K=3072, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+
+    // --- Layer 1 --- (Repeat structure)
+    Layer Enc1_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc1_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc1_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc1_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc1_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc1_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 2 --- (Repeat structure)
+    Layer Enc2_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc2_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc2_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc2_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc2_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc2_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 3 --- (Repeat structure)
+    Layer Enc3_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc3_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc3_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc3_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc3_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc3_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 4 --- (Repeat structure)
+    Layer Enc4_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc4_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc4_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc4_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc4_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc4_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 5 --- (Repeat structure)
+    Layer Enc5_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc5_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc5_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc5_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc5_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc5_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 6 --- (Repeat structure)
+    Layer Enc6_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc6_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc6_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc6_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc6_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc6_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 7 --- (Repeat structure)
+    Layer Enc7_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc7_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc7_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc7_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc7_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc7_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 8 --- (Repeat structure)
+    Layer Enc8_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc8_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc8_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc8_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc8_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc8_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 9 --- (Repeat structure)
+    Layer Enc9_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc9_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc9_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc9_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc9_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc9_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 10 --- (Repeat structure)
+    Layer Enc10_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc10_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc10_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc10_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc10_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc10_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+    // --- Layer 11 --- (Repeat structure)
+    Layer Enc11_Attn_Q { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc11_Attn_K { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc11_Attn_V { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc11_Attn_Output { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc11_FFN1 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 3072, C: 768, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+    Layer Enc11_FFN2 { 
+        Type: CONV 
+        Stride { X: 1, Y: 1 } 
+        Dimensions { K: 768, C: 3072, R: 1, S: 1, Y: 197, X: 1 } 
+    }
+
+
+// === Highway Exits (0-3) ===
+// Input to Pooler layers (as CONV): Y=1, X=1
+
+    // --- Highway 0 ---
+    Layer Highway0_Pooler { // Represents: Pooler Linear(768, 768) -> GEMM M=1, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+    // Highway0_Classifier (Identity) omitted.
+
+    // --- Highway 1 ---
+    Layer Highway1_Pooler { // Represents: Pooler Linear(768, 768) -> GEMM M=1, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+    // Highway1_Classifier (Identity) omitted.
+
+    // --- Highway 2 ---
+    Layer Highway2_Pooler { // Represents: Pooler Linear(768, 768) -> GEMM M=1, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+    // Highway2_Classifier (Identity) omitted.
+
+    // --- Highway 3 ---
+    Layer Highway3_Pooler { // Represents: Pooler Linear(768, 768) -> GEMM M=1, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+    // Highway3_Classifier (Identity) omitted.
+
+
+// === Final Exit ===
+// Input to Final Pooler layer (as CONV): Y=1, X=1
+
+    Layer Final_Pooler { // Represents: Final Pooler Linear(768, 768) -> GEMM M=1, K=768, N=768
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 768, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=N_gemm, C=K_gemm, Y=M_gemm
+
+    }
+    // Final_Classifier (Identity) omitted. Add CONV layer if needed for actual classifier.
+
+    Layer Final_Classifier { // Represents: Final Linear(768, 1000) -> GEMM M=1, K=768, N=1000
+        Type: CONV
+        Stride { X: 1, Y: 1 }
+        Dimensions { K: 1000, C: 768, R: 1, S: 1, Y: 1, X: 1 } // K=NumClasses, C=InFeat, Y=1, X=1
+
+    }
+
+} // End Network DeiTHighway
